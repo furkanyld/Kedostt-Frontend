e9fe9c7b36c814c56714347e0fa6e044c8fd8b2a
furkanyld <furkan16_yildiz@hotmail.com>
2025-07-01 15:24:29 +0300

    index.html fixed.

--- src/Api/PrivateAxios.js
+++ src/Api/PrivateAxios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.REACT_APP_BACKEND_URL || "http://localhost:8080";
+const BASE_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:8080";
 
 const privateAxios = axios.create({
   baseURL: BASE_URL,
