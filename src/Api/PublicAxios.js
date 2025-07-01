cb2a0fa928d54f6aeb7e4f70e0f4873a7e932751
furkanyld <furkan16_yildiz@hotmail.com>
2025-07-01 14:29:40 +0300

    Ready to deploy

--- src/Api/PublicAxios.js
+++ src/Api/PublicAxios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:8080";
+
 const publicAxios = axios.create({
-  baseURL: "http://localhost:8080", 
+  baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
